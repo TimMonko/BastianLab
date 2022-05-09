ff67f0164720843c9ec1dd3c82858c9173554903
Tim-Monko <47310455+TimMonko@users.noreply.github.com>
2022-05-09 12:28:44 -0500

    Update Dask-CZI-BigScreening.py

--- LearningPythonPipelines/Dask-CZI-BigScreening.py
+++ LearningPythonPipelines/Dask-CZI-BigScreening.py
@@ -38,13 +38,15 @@ from aicsimageio import AICSImage
 img = AICSImage(filepath_list[0])
 print(img.dims)
 
-if 'viewer' not in globals():
-    viewer = napari.Viewer()
-
 import pyclesperanto_prototype as cle
 cle.get_device()
 
 print("Import: ", time.time() - start)
+
+if 'viewer' not in globals():
+    viewer = napari.Viewer()
+    
+!nvidia-smi --query-gpu=memory.used --format=csv
 
 #%% Ridge Filtering
 
@@ -54,32 +56,32 @@ cle.push(C0)
 C0_crop = cle.crop(C0, width = 1000, height = 1000, start_x = 7000, start_y = 7000)
 viewer.add_image(C0_crop)
 
-C0_blur = cle.gaussian_blur(C0_crop, None, 1, 1, 0)
-viewer.add_image(C0_blur)
+#C0_blur = cle.gaussian_blur(C0_crop, None, 1, 1, 0)
+#viewer.add_image(C0_blur)
 
 from skimage.filters import meijering #, sato, frangi, hessian
 
-C0_sgb = cle.subtract_gaussian_background(C0_blur, None, 10.0, 10.0, 0.0)
-viewer.add_image(C0_sgb)
-C0_mj = meijering(C0_sgb, sigmas = range(6,12,2), black_ridges = False) # Meijering Ridge Filter
+C0_mj = meijering(C0_crop, sigmas = range(1,5,1), black_ridges = False) # Meijering Ridge Filter
 viewer.add_image(C0_mj)
+
+C0_neurites = cle.greater_constant(C0_mj, None, constant = 0.04) #second order image threshold
+viewer.add_labels(C0_neurites)
+
+print("Neurite Filtering: ", time.time() - start)
 
 #%% DAPI Filtering
 C1 = img.get_image_dask_data("YX", C=1)
 
 C1_crop = cle.crop(C1, width = 1000, height = 1000, start_x = 7000, start_y = 7000)
+viewer.add_image(C1_crop)
 
-C1_blur = cle.gaussian_blur(C1_crop, None, 3.0, 3.0, 0.0)
-
-C1_ths = cle.top_hat_sphere(C1_blur, None, 20.0, 20.0, 0.0)
-
-C1_DAPI = cle.voronoi_otsu_labeling(C1_ths, spot_sigma = 10)
+C1_DAPI = cle.voronoi_otsu_labeling(C1_crop, spot_sigma = 10)
+!nvidia-smi --query-gpu=memory.used --format=csv
 viewer.add_labels(C1_DAPI)
-
 
 print("Blob Filtering: ",time.time() - start)
 
-#%% Blob Labelling
+#%% Blob Labelling - redo this to work with Big imagE? 
 
 C0_max = cle.detect_maxima_box(C0_lb, radius_x = 2, radius_y = 2, radius_z = 0) #Local Maxima, automatic
 C0_otsu = cle.threshold_otsu(C0_lb)
@@ -88,45 +90,3 @@ C0_blobs = cle.masked_voronoi_labeling(C0_spots, C0_otsu) # Convert binary map t
 
 viewer.add_labels(C0_blobs)
 print("Blob Labeling: ", time.time() - start)
-
-#%% Ridge Filtering
-
-from skimage.filters import meijering #, sato, frangi, hessian
-
-C0s_gb = cle.gaussian_blur(C0, None, 1.0, 1.0, 0.0)# Gaussian Blur 
-C0s_sgb = cle.subtract_gaussian_background(C0s_gb, None, 10.0, 10.0, 0.0) # Gaussian Background Subtraction
-C0s_mj = meijering(C0s_sgb, sigmas = range(6,12,2), black_ridges = False) # Meijering Ridge Filter
-
-viewer.add_image(C0s_mj)
-print("Ridge Filtering: ", time.time() - start)
-
-#%% Ridge Labelling
-
-C0s_mj_to = cle.threshold_otsu(C0s_mj) # Otsu of Ridges, binary
-C0s_mj_cl = cle.closing_labels(C0s_mj_to, None, 3.0) # Closing Labels, binary
-C0s_neurites = cle.connected_components_labeling_box(C0s_mj_cl) # Connected Components Labeling, CCM
-
-viewer.add_labels(C0s_neurites)
-print("Ridge Labelling: ", time.time() - start)
-
-#%% Ridge Signed Maurer Distance Map 
-
-import napari_simpleitk_image_processing as nsitk
-
-not_neurite = cle.binary_not(C0s_mj_to)
-distance_from_neurite = nsitk.signed_maurer_distance_map(not_neurite)
-
-mean_distance_map = cle.mean_intensity_map(distance_from_neurite, C0_blobs)
-
-viewer.add_image(mean_distance_map, colormap = 'turbo')
-print("Signed Maurer Distance Map: ", time.time() - start)
-#%% Filter Blobs by SMD Map
-
-objects_close_by_neurite = cle.exclude_labels_with_map_values_out_of_range(
-    mean_distance_map,
-    C0_blobs,
-    minimum_value_range=-100,
-    maximum_value_range=5)
-
-viewer.add_image(objects_close_by_neurite, colormap= 'turbo')
-print("Filter Blobs by SMD Map: ", time.time() - start)
