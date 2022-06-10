4c9e076455312cc30732a99d25b39061dc0cfc2d
Tim-Monko <47310455+TimMonko@users.noreply.github.com>
2022-06-10 17:07:15 -0500

    Update PunctaStats.py

--- WorkingPython/PunctaProject/PunctaStats.py
+++ WorkingPython/PunctaProject/PunctaStats.py
@@ -8,29 +8,80 @@ https://www.utc.fr/~jlaforet/Suppl/python-cheatsheets.pdf
 https://stackoverflow.com/questions/44065573/anova-for-groups-within-a-dataframe-using-scipy
 """
 #%% Import and Manipulate Data
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filepath = "C:\\Users\Tim M\Downloads\label_summary.csv"
+file_directory = "C:\\Users\TimMonko\Downloads"
+os.chdir(file_directory)
+
+filepath = "C:\\Users\TimMonko\Downloads\label_summary.csv"
 raw_data = pd.read_csv(filepath)
 
-data = (raw_data.assign(treatment = lambda x: raw_data.filename.str.extract(r'(Group\d)'))
+
+data = (raw_data
+        .assign(treatment = lambda x: raw_data.filename.str.extract(r'(Group\d)'))
+        .replace({'Group1': 'Iron Sufficient', 'Group2': 'Iron Repleted', 'Group3': 'Iron Deficient'})
         .dropna() # drop rows with any column having NA/null, can index
         ) 
+
+print("number of files without full analysis:", len(raw_data)-len(data))
+
 data_grp = data.groupby("treatment")
-
 data_describe = data_grp.describe()
+print(data_describe)
 
-#%% Seaborn Plotting
+#print("number of files for each group: \n", data.groupby('treatment').size())
+
+
+#%% Seaborn Settings
 import seaborn as sns
 
-ax = sns.boxplot(x = 'treatment', y = 'blob_per_squm', data = data)
-ax = sns.swarmplot(x = 'treatment', y = 'blob_per_squm', data = data)
-plt.xlabel("Treatment Group")
-plt.ylabel("Puncta / Sq.um")
-plt.show()
-print("number of files without full analysis:", len(raw_data)-len(data))
-print("number of files for each group: \n", data.groupby('treatment').size())
+# must explicitely invoke set_them else uses matplotlib defaults. Overrides all matplotlib based plots
+sns.set_theme(
+    style = "darkgrid",
+    context = "notebook",
+    palette = "colorblind",
+    ) # explicility invoked else uses matplotlib defaults
+# plt.style.use() # 
+
+#%% Discrete plotting
+
+ax = sns.boxplot(x = 'treatment',
+                 y = 'blob_per_squm',
+                 data = data,
+                 width = 0.6,
+                 palette = 'pastel'
+                 )
+
+ax = sns.swarmplot(x = 'treatment',
+                   y = 'blob_per_squm',
+                   data = data,
+                   edgecolor = 'gray',
+                   linewidth = None
+                   )
+
+ax.set_xlabel(None)
+ax.set_ylabel("Puncta / Sq.um")
+
+ax.figure # call figure without plt.show()
+ax.figure.savefig('BoxPlot.svg') # save figure object from ax class
+
+
+#%% Seaborn Relational Plotting
+
+r = sns.lmplot(x = 'ridge_squm', 
+               y = 'total_blobs', 
+               hue = 'treatment', 
+               data = data)
+
+r.legend.set_title("Condition")
+r.set_xlabels("Neurite, Sq.um")
+r.set_ylabels("Puncta")
+
+r.figure
+r.figure.savefig('LmPlot.svg')
+
 
 #%% Tiday Day statsmodels stats https://www.reneshbedre.com/blog/anova.html
 
